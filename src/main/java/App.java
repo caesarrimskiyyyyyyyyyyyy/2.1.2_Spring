2722a3fc68c99fa11661b744f8587d0e8d396da6
caesarrimskiyyyyyyyyyyyy <caesarrimskiy@gmail.com>
2024-09-02 22:36:04 +0300

    finish

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,14 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+
+        HelloWorld firstHelloWorld = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld secondHelloWorld = (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat firstCatBean = (Cat) applicationContext.getBean("cat");
+        Cat secondCatBean = (Cat) applicationContext.getBean("cat");
+
+        System.out.println("helloworld bean: " + (firstHelloWorld == secondHelloWorld));
+        System.out.println("cat bean: " + (firstCatBean == secondCatBean));
     }
 }
